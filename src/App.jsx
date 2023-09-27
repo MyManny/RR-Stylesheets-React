f9b7ec54a066adb7a5d2ef87700978573d377efa
MyManny <gmail.com>
2023-09-26 20:56:47 -0700

    inline styling

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+import SearchBar from './Components/SearchBar'
+import UserGallery from './Components/UserGallery'
+
+const appStyle = {
+  'background-color': '#a6a6a6',
+  'min-height': '100vh',
+}
+
+const searchBarStyle = {
+  'width': '500px',
+  'border-radius': '50px',
+  'border': '2px solid black',
+  'margin-top': '20px',
+  'padding': '5px'
+}
+
+function App() {
+  return (
+    <div className="App" style={appStyle}>
+      <SearchBar searchBarStyle={searchBarStyle} />
+      <UserGallery />
+    </div>
+  );
+}
+
+export default App;
